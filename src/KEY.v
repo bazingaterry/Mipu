97314fc78687916b64040ad3b23bf5bfd25184af
bazingaterry <531410054@qq.com>
2016-03-24 22:30:30 +0800

    修正过度消抖

--- src/KEY.v
+++ src/KEY.v
@@ -1,22 +1,22 @@
 module KEY (
-	input wire BJ_CLK, // 3MHz Clock in
+	input wire BJ_CLK,
 	input wire RESET,
 	input wire BUTTON_IN,
 	output wire BUTTON_OUT
 );
 
-reg [21:0] BJ_CLK_DIV; // 40Hz
+reg [15:0] BJ_CLK_DIV;
 
 always @ (posedge BJ_CLK or posedge RESET) begin
 	if (RESET)
 		BJ_CLK_DIV <= 0;
-	else 
+	else
 		BJ_CLK_DIV <= BJ_CLK_DIV + 1'b1;
 end
 
 reg BUTTON_IN_Q, BUTTON_IN_2Q, BUTTON_IN_3Q;
 
-always @ (posedge BJ_CLK_DIV[21] or posedge RESET) begin
+always @ (posedge BJ_CLK_DIV[15] or posedge RESET) begin
 	if(RESET) begin
 		BUTTON_IN_Q <= 1'b1;
 		BUTTON_IN_2Q <= 1'b1;
